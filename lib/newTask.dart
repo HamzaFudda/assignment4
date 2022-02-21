34f94ea41adb74eaf81f677756aabd832320440a
HamzaFudda <humzafudda31@gmail.com>
2022-02-21 16:00:22 +0500

    1

--- lib/newTask.dart
+++ lib/newTask.dart
@@ -2,6 +2,7 @@ import 'package:assignment4/todo.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
 
 class newtask extends StatefulWidget {
   const newtask({Key? key}) : super(key: key);
@@ -11,8 +12,8 @@ class newtask extends StatefulWidget {
 }
 
 class _newtaskState extends State<newtask> {
-  TextEditingController _title = TextEditingController();
-  TextEditingController _description = TextEditingController();
+  TextEditingController titleMainController = TextEditingController();
+  TextEditingController descriptionMainController = TextEditingController();
   DateTime _selectedDate = DateTime.now();
 
   @override
@@ -24,53 +25,15 @@ class _newtaskState extends State<newtask> {
       body: Container(
         child: Column(
           children: <Widget>[
-            SizedBox(
-              height: 50,
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(24, 10, 24, 30),
-              child: TextField(
-                controller: _title,
-                style: TextStyle(color: Colors.black),
-                decoration: InputDecoration(
-                    fillColor: Colors.grey.shade100,
-                    filled: true,
-                    hintText: "Title",
-                    disabledBorder: InputBorder.none,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    )),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(24, 10, 24, 30),
-              child: TextField(
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                controller: _description,
-                style: TextStyle(color: Colors.black, fontSize: 20),
-                decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.fromLTRB(12, 20, 12, 70),
-                    fillColor: Colors.grey.shade100,
-                    filled: true,
-                    hintText: "Description",
-                    disabledBorder: InputBorder.none,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    )),
-              ),
-            ),
+            SizedBox(height: 50),
+            title(titleController: titleMainController),
+            description(descriptionController: descriptionMainController),
             datePicker(
-              selectedDate: _selectedDate,
-              onPressedUpdate: (recieved_date) {
-                _selectedDate = recieved_date;
-                setState(() {});
-              },
-            ),
-            Text("$_selectedDate"),
-            SizedBox(
-              height: 20,
-            ),
+                selectedDate: _selectedDate,
+                onPressedUpdate: (recieved_date) {
+                  _selectedDate = recieved_date;
+                }),
+            SizedBox(height: 20),
             ElevatedButton(onPressed: () {}, child: Text("Add Task"))
           ],
         ),
@@ -92,17 +55,40 @@ class datePicker extends StatefulWidget {
 }
 
 class _datePickerState extends State<datePicker> {
+  TextEditingController _date = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        _selectDate(context);
-      },
-      child: Text("Choose Date"),
+    return Column(
+      children: [
+        Text(DateFormat('dd-MMM-yyyy').format(widget.selectedDate)),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(24, 10, 24, 30),
+          child: TextField(
+            controller: _date,
+            style: TextStyle(color: Colors.black),
+            decoration: InputDecoration(
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.arrow_circle_right),
+                  onPressed: () async {
+                    await _selectDate(context);
+                    _date.text = DateFormat('dd-MMM-yyyy').format(widget.selectedDate);
+                    setState(() {});
+                  },
+                ),
+                fillColor: Colors.grey.shade100,
+                filled: true,
+                hintText: "Due date",
+                disabledBorder: InputBorder.none,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                )),
+          ),
+        ),
+      ],
     );
   }
 
-  _selectDate(BuildContext context) async {
+  Future <void> _selectDate(BuildContext context) async {
     final DateTime? selected = await showDatePicker(
       context: context,
       initialDate: widget.selectedDate,
@@ -115,5 +101,59 @@ class _datePickerState extends State<datePicker> {
       });
       widget.onPressedUpdate(widget.selectedDate);
     }
+  }
+}
+
+class title extends StatelessWidget {
+  TextEditingController titleController = TextEditingController();
+
+  title({Key? key, required this.titleController}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(24, 10, 24, 30),
+      child: TextField(
+        controller: titleController,
+        style: TextStyle(color: Colors.black),
+        decoration: InputDecoration(
+            fillColor: Colors.grey.shade100,
+            filled: true,
+            hintText: "Title",
+            disabledBorder: InputBorder.none,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+            )),
+      ),
+    );
+  }
+}
+
+class description extends StatelessWidget {
+  TextEditingController descriptionController = TextEditingController();
+
+  description({Key? key, required this.descriptionController})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(24, 10, 24, 30),
+      child: TextField(
+        keyboardType: TextInputType.multiline,
+        maxLines: null,
+        controller: descriptionController,
+        style: TextStyle(color: Colors.black, fontSize: 20),
+        decoration: InputDecoration(
+            contentPadding: const EdgeInsets.fromLTRB(12, 20, 12, 70),
+            fillColor: Colors.grey.shade100,
+            filled: true,
+            hintText: "Description",
+            disabledBorder: InputBorder.none,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+            )),
+      ),
+    );
   }
 }
